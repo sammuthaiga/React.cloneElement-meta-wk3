cc8dda3cb7247d411dc94be973604b73ac29f009
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-07-31 02:05:27 +0300

    transform spacing using the React.Children, React.cloneElement APIs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import  React from "react";
+import "./App.css"
+
+
+
+const Row = ({ children, spacing }) => {
+const childStyle = {
+  marginLeft: `${spacing}px`,
+}
+
+  return (
+    <div className="Row">
+  {React.Children.map(children, (child, index) => {
+    return React.cloneElement(child, {
+      style: {
+        ...child.props.style,
+        ...(index > 0 ? childStyle : {}),
+      },
+    }) 
+  })}
+</div>
+  )
+}
 
 function App() {
   return (
-    <div>Great Day It Is!!!</div>
+    <div className="App">
+      <Row spacing={32}>
+        <p>Pizza Margarita</p>
+        <p>2</p>
+        <p>30$</p>
+        <p>18:30</p>
+        <p>Sam</p>
+      </Row>
+      <p>Hello do you see me?</p>
+
+    </div>
   );
 }
 
